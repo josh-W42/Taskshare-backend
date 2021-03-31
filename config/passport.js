1979a6f6ac772080262c7c2dff05c4fba1130f01
jwilson42 <wilson.jd022@gmail.com>
2021-03-30 22:30:26 -0700

    setup login register routes

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
-// A passport strategy for authenticating with a JSON Web Token
-// This allows to authenticate endpoints using a token
 
-// As opposed to in our Sequelize app with auth,
-// We'll be using the JSON Web Token Strategy as opposed to Local Strategy
 const { Strategy, ExtractJwt }  = require('passport-jwt');
 const mongoose = require('mongoose');
 
@@ -15,17 +11,11 @@ options.secretOrKey = process.env.JWT_SECRET;
 
 module.exports = passport => {
     passport.use(new Strategy(options, async (jwt_payload, done) => {
-        //  We want to find a use by an id that is inside the jwt_payload
-        // When we get that use back, we check to see if the user is in database.
         try {
             const user = await User.findById(jwt_payload.id);
-            // jwt_payload is an object that contains JWT info
-            // done is callback that we use to return user or false.
-            if (user) {
-                return done(null, user);
-            } else {
-                throw new Error('User Not Found');
-            }
+            if (!user) throw new Error('User Not Found');
+
+            return done(null, user);
         } catch (error) {
             console.error(error);
             return done(null, false);
--- controllers/user.js
+++ controllers/user.js
@@ -1,107 +1,128 @@
 // imports
-require('dotenv').config();
-const passport = require('passport');
-// For now we'll use bcrypt but ill do a scrypt example too
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
 // Data base
-const db = require('../models');
+const db = require("../models");
 
-// basic test 
+// basic test
 const test = async (req, res) => {
-  res.json({ message: 'User endpoint OK!' });
-}
+  res.json({ message: "User endpoint OK!" });
+};
 
-// Controller for Registering a new User.
+// Registering a new User. And Log in
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
   // Ok so this is when do things like check if an email already exists
   try {
     const user = await db.User.findOne({ email });
-
-    // But What about mongo validation? we could use the unique: true
-    if (user) throw new Error('Email already exists');
+    // If found, throw an error.
+    if (user) throw new Error("Email already exists");
 
     // Create a new user.
-    const newUser = await db.User.create({ name, email, password });
+    const newUser = await db.User.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
 
     // Salt and hash the password.
     bcrypt.genSalt(12, (error, salt) => {
-      if (error) throw new Error('Salt Generation Failed');
+      if (error) throw new Error("Salt Generation Failed");
 
       bcrypt.hash(newUser.password, salt, async (error, hash) => {
-        if (error) throw new Error('Hash Password Failure');
+        if (error) throw new Error("Hash Password Failure");
 
         newUser.password = hash;
         const createdUser = await newUser.save();
-        res.json(createdUser);
+
+        // add the new username to autocomplete index
+        // Trie.addWord(userName.toLowerCase(), "user");
+        // Not yet.
+
+        // We have to log in
+        const payload = {
+          id: createdUser._id,
+          email: createdUser.email,
+        };
+
+        jwt.sign(payload, JWT_SECRET, (error, token) => {
+          if (error) throw new Error("Session has ended, please log in again");
+
+          const legit = jwt.verify(token, JWT_SECRET, { expiresIn: 60 });
+
+          res.status(201).json({
+            success: true,
+            token: `Bearer ${token}`,
+            data: legit,
+            message: "User Created",
+          });
+        });
       });
     });
   } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error.message });
+    if (error.name === "MongoError") {
+      const needToChange = error.keyPattern;
+      res.status(409).json({
+        success: false,
+        message: "Database Error",
+        needToChange,
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
   }
-}
+};
 
-// Controller for logging in.
+// User logging in.
 const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     // First find user.
     const findUser = await db.User.findOne({ email });
-    if (!findUser) throw new Error('User not found.');
+    if (!findUser) throw new Error("User not found.");
 
     // Second compare the user password/
     const isValid = await bcrypt.compare(password, findUser.password);
-    if (!isValid) throw new Error('Incorrect Password');
+    if (!isValid) throw new Error("Incorrect Password");
 
-    // If the password is valid, then we send off the json web token payload
+    // Log in
     const payload = {
       id: findUser.id,
       email: findUser.email,
-      name: findUser.name
-    }
+    };
 
-    /* 
-      OK so a note, in one hour the backend will basically log you out
-      Now idk about you but depending on what you're building you'd want to change this.
+    jwt.sign(payload, JWT_SECRET, (error, token) => {
+      if (error) throw new Error("Session has ended, please log in again");
 
-      Pros:
-        You're protecting your users data and you API.
-        This is very useful for protecting sensitve information.
-
-      Cons:
-        If your user is active on the site and you reach the one hour mark, it'll log them out. Which can be a frustrating UX.
-        Is there really any other cons other than a frustrating UX?
-
-      Ultimately, it's up to you and your team at the end of the day.
-    */
-    jwt.sign(payload, JWT_SECRET, { expiresIn: 3600 }, (error, token) => {
-      if (error) throw new Error('Session has ended, please log in again');
-      
-      // This verify method expires in 60 seconds if there is no response after attempting to verify the token
       const legit = jwt.verify(token, JWT_SECRET, { expiresIn: 60 });
 
       res.json({ success: true, token: `Bearer ${token}`, userData: legit });
     });
-    
   } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error.message});
+    res.status(400).json({ success: false, message: error.message });
   }
-}
+};
 
 // Get Profile information
 const profile = (req, res) => {
-  const { id, name, email } = req.user;
-  res.json({ id, name, email });
-}
+  
+  
+};
 
 // export all route functions
 module.exports = {
-  test, register, login, profile
-}
+  test,
+  register,
+  login,
+  profile,
+};
