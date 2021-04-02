a03695679a4c0223824fbbb7e136e5f1b3c2d710
jwilson42 <wilson.jd022@gmail.com>
2021-04-01 23:28:42 -0700

    added memberget routes and controllers when searcher is a member vs admin

--- controllers/member.js
+++ controllers/member.js
@@ -9,7 +9,78 @@ const test = async (req, res) => {
   res.json({ message: "Members endpoint OK!" });
 }
 
+// member creation is handled in the user and workspace level.
+
+// get member data if a member is searching
+const readOneMember = async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    // check member existence
+    const foundMember = await db.Member.findOne({ _id }).select("-permissions -rooms -userId");
+    if (!foundMember) throw new Error("Member Does Not Exist");
+
+    // check if searcher is a member
+    const [type, token] = req.headers.authorization.split(' ');
+    const payload = jwt.decode(token);
+
+    const searchMember = await db.Member.findOne({ userId: payload.id, workspaceId: foundMember.workspaceId });
+    if (!searchMember) throw new Error("Forbidden");
+
+    // Return data
+    res.json({ success: true, result: foundMember });
+
+  } catch (error) {
+    if (error.message === "Forbidden") {
+      res.status(403).json({
+        success: false,
+        message: "You Are Not a Member Of This Workspace.",
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  } 
+}
+
+const readOneAdmin = async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    // check member existence
+    const foundMember = await db.Member.findOne({ _id });
+    if (!foundMember) throw new Error("Member Does Not Exist");
+
+    // check if searcher is admin
+    const [type, token] = req.headers.authorization.split(' ');
+    const payload = jwt.decode(token);
+
+    const searchMember = await db.Member.findOne({ userId: payload.id, workspaceId: foundMember.workspaceId });
+    if (!searchMember || !searchMember.role.includes('admin')) throw new Error("Forbidden");
+
+    // return the data
+    res.json({ success: true, result: foundMember });
+
+  } catch (error) {
+    if (error.message === "Forbidden") {
+      res.status(403).json({
+        success: false,
+        message: "You Are Not An Admin Of This Workspace.",
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+}
+
 // export all route functions
 module.exports = {
   test,
+  readOneMember,
+  readOneAdmin,
 }
--- routes/member.js
+++ routes/member.js
@@ -3,5 +3,15 @@ const ctrl = require('../controllers');
 const passport = require('passport');
 
 router.get('/test', ctrl.member.test);
+router.get(
+  "/:id/member",
+  passport.authenticate('jwt', { session: false }),
+  ctrl.member.readOneMember,
+);
+router.get(
+  "/:id/admin",
+  passport.authenticate('jwt', { session: false }),
+  ctrl.member.readOneAdmin,
+);
 
 module.exports = router;
